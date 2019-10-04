75b3f99de119208fcb77d8a2551d562d90316c33
abhishek shrivastava <abhishekshrivastava78@gmail.com>
2019-10-04 13:51:29 +0530

    Xylo phone finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XyloPhone());
 
 
 
 class XyloPhone extends StatelessWidget {
+  Widget createMusicButton(Color buttonColor, int note) {
+    return Expanded(
+      child: FlatButton(
+        color: buttonColor,
+        onPressed: () {
+          AudioCache player = AudioCache();
+          player.play('note$note.wav');
+        },
+        child: Text(''),
+      ),
+
+    );
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: FlatButton(
-              color: Colors.purple.shade300,
-              child: Text('Play Sound'),
-              onPressed: (){},
-            ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisSize: MainAxisSize.max,
+
+            children: <Widget>[
+              createMusicButton(Colors.red, 1),
+              createMusicButton(Colors.orange, 2),
+              createMusicButton(Colors.yellow, 3),
+              createMusicButton(Colors.green, 4),
+              createMusicButton(Colors.blue, 5),
+              createMusicButton(Colors.indigo, 6),
+              createMusicButton(Colors.purple, 7),
+            ],
           ),
         ),
       ),
     );
   }
 }
-
